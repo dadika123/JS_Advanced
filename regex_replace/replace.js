709202bef46f1574ff4314ab7d967eb3f9a31bd0
chuzhikov_danil <danil.chuzhikov@gmail.com>
2021-03-25 17:39:47 +0300

    homework_5

--- regex_replace/replace.js
+++
@@ -1,4 +0,0 @@
-let text = document.getElementById('text');
-document.getElementById('replace').addEventListener('click', ()=>{
-    text.textContent = text.textContent.replace(/\B'|'\B/g, '"');
-});
